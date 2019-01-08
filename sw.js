4582f6c21dda0fbc88d7c78ae36fe63da0dbc9aa
Luis Fernando <fernando.hurtado@vml.com>
2019-01-08 16:06:23 -0600

    modificaciones de iphone x y mas

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports utils
 importScripts('js/sw-utils.js');
 
-const CACHE_STATIC = "static-v2";
-const CACHE_DYNAMIC = "dynamic-v1";
+const CACHE_STATIC = "static-v3";
+const CACHE_DYNAMIC = "dynamic-v2";
 const CACHE_INMUTABLE = "inmutable-v1";
 const APP_SHELL =[
 	// '/',
@@ -34,6 +34,9 @@ self.addEventListener('activate', e =>{
 	const oldCaches = caches.keys().then(keys=>{
         keys.forEach(key => {
             if(key !== CACHE_STATIC && key.includes('static-')){
+                return caches.delete(key);
+            }
+            if(key !== CACHE_DYNAMIC && key.includes('dynamic-')){
                 return caches.delete(key);
             }
         })
